030d4fa1b9c3dbec05c64b8d5dd718489ad63b4f
eduardobcoelho <luizbcoelho27@outlook.com>
2025-03-12 23:59:39 -0300

    fix: added DefaultGenderPipe dependency

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -1,8 +1,7 @@
 import { Module } from '@nestjs/common';
-import { DefaultGenderPipe } from './pipes/default-gender/default-gender.pipe';
 
 @Module({
-  providers: [DefaultGenderPipe],
-  exports: [DefaultGenderPipe],
+  providers: [],
+  exports: [],
 })
 export class CommonModule {}
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { CommonModule } from 'src/common/common.module';
+import { DefaultGenderPipe } from './pipes/default-gender/default-gender.pipe';
 
 @Module({
   imports: [CommonModule],
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [UsersService, DefaultGenderPipe],
   exports: [UsersService],
 })
 export class UsersModule {}

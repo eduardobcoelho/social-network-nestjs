64efe2ea5c7c68d89b77ad59ff9557eb674b318d
eduardobcoelho <luizbcoelho27@outlook.com>
2025-04-01 00:31:23 -0300

    fix: adjusted dependency injection

--- src/posts/service/create-post/create-post.service.ts
+++ src/posts/service/create-post/create-post.service.ts
@@ -2,7 +2,7 @@ import { Inject, Injectable } from '@nestjs/common';
 import { CreatePostDto } from 'src/posts/dto/create-post.dto';
 import { PostEntity } from 'src/posts/entity/post.entity';
 import { IPostRepository } from 'src/posts/repository/post.repository';
-import { FindUserService } from 'src/users/service/find-user/find-user.service';
+import { IFindUserService } from 'src/users/service/find-user/find-user.service';
 
 export interface ICreatePostService {
   exec: (data: CreatePostDto) => Promise<PostEntity>;
@@ -14,7 +14,7 @@ export class CreatePostService implements ICreatePostService {
     @Inject('IPostRepository') private readonly postRepository: IPostRepository,
 
     @Inject('IFindUserService')
-    private readonly findUserService: FindUserService,
+    private readonly findUserService: IFindUserService,
   ) {}
 
   async exec(data: CreatePostDto) {

98f48d309593f26fb5ea0054fc1bdbb67d0858d3
eduardobcoelho <luizbcoelho27@outlook.com>
2025-04-04 00:51:55 -0300

    feat: created update comment service

--- src/comments/service/update-comment/update-comment.service.ts
+++ src/comments/service/update-comment/update-comment.service.ts
@@ -1,15 +1,38 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { CommentEntity } from 'src/comments/entity/comment.entity';
+import { ICommentRepository } from 'src/comments/repository/comment.repository';
 import { UpdatePostDto } from 'src/posts/dto/update-post.dto';
+import { IFindCommentService } from '../find-comment/find-comment.service';
+import { UpdateCommentDto } from 'src/comments/dto/update-comment.dto';
 
 export interface IUpdateCommentService {
-  exec: (data: UpdatePostDto) => Promise<CommentEntity>;
+  exec: (id: number, data: UpdatePostDto) => Promise<CommentEntity>;
 }
 
 @Injectable()
 export class UpdateCommentService implements IUpdateCommentService {
-  exec(data: UpdatePostDto) {
-    console.log(data);
-    return Promise.resolve({} as CommentEntity);
+  constructor(
+    @Inject('ICommentRepository')
+    private readonly commentRepository: ICommentRepository,
+
+    @Inject('IFindCommentService')
+    private readonly findCommentService: IFindCommentService,
+  ) {}
+
+  async exec(id: number, data: UpdateCommentDto) {
+    await this.findCommentService.exec(id);
+
+    const dtoValidKeys: Array<keyof UpdateCommentDto> = ['text'];
+    const dataSerialized = dtoValidKeys.reduce((acc, cur) => {
+      if (data[cur]) {
+        acc[cur] = data[cur];
+      }
+      return acc;
+    }, {});
+
+    return await this.commentRepository.update(
+      id,
+      dataSerialized as UpdateCommentDto,
+    );
   }
 }
